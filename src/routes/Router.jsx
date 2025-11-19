8d774c7b4748bfb932397e21d303f2958d1bcfd1
student-Imran <ahmedimranmd55@gmail.com>
2025-11-19 22:55:59 +0600

    Coverage area added

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -14,7 +14,8 @@ export const router = createBrowserRouter([
         },
         {
             path : '/coverage',
-            Component : Coverage
+            Component : Coverage,
+            loader: () => fetch('/warehouses.json').then(res=>res.json())
         }
     ]
   },
